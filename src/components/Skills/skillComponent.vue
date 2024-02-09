f0b4a745e4d0e8b222b8de8cbe294e7da424ea21
Ahmed Abdulrahman <YOUR_GITHUB_EMAIL_ADDRESS>
2024-02-09 08:56:39 +0100

    edit

--- src/components/Skills/skillComponent.vue
+++ src/components/Skills/skillComponent.vue
@@ -3,7 +3,7 @@
         <div class="w-fit">
             <div class="rounded-full border-2 border-black bg-slate-300 w-fit m-auto">
                 <div class="grid w-fit m-auto justify-center p-2">
-                    <img class="w-7 md:w-10" :src="`/${svg}.svg`" alt="">
+                    <img class="w-4 md:w-6" :src="`/${svg}.svg`" alt="">
                 </div>
             </div>
         </div>
