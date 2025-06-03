cad4e0859449cd22e5c3b506a9e1f7e748a595aa
Ahmed Abdulrahman <104634941+abdurrahmanu@users.noreply.github.com>
2025-06-03 05:15:50 +0100

    Update PopUp.vue

--- src/components/PopUp.vue
+++ src/components/PopUp.vue
@@ -1,12 +1,9 @@
 <template>
     <div class="max-w-[600px] bg-neutral-700 text-white dark:bg-neutral-800 p-4 px-6">
-        <p class="text-center md:text-xl p-3">YOU WILL BE REDIRECTED TO VIEW MY RESUME</p>
-        <div class="flex gap-10 w-fit p-1 m-auto">
+        <p class="text-center md:text-xl p-3">NOT AVAILABLE NOW</p>
+        <div class="flex justify-center gap-10 w-fit p-1 m-auto">
             <div class="text-center">
-                <button @click="$emit('toggleResume', 'yes')" class="px-6 rounded-full p-[2px] text-white bg-green-500">YES</button>
-            </div>
-            <div class="text-center">
-                <button @click="$emit('toggleResume', 'no')" class="px-6 rounded-full p-[2px] text-white bg-red-400">NO</button>
+                <button @click="$emit('toggleResume', 'no')" class="px-6 rounded-full p-[2px] text-white bg-red-400">CLOSE</button>
             </div>
         </div>
     </div>
